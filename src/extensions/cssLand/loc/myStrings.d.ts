61fb1fbdcec15e5901298ed047da91cc9d2af407
rezmes <rezmes@gmail.com>
2025-01-14 11:22:27 +0330

    yo @microsoft/sharepoint

---
+++ src/extensions/cssLand/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ICssLandApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'CssLandApplicationCustomizerStrings' {
+  const strings: ICssLandApplicationCustomizerStrings;
+  export = strings;
+}
